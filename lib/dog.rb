7f5f89bad7e130aca63cfe6564a4da1f3ff145c6
JC <bonassin@gmail.com>
2020-12-28 17:49:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 class Dog
-  def initialize(breed)
-    @breed = breed = ("Mutt")
+  def initialize(name)
+    @name = name
   end
 
-  def breed=(breed)
-    @breed = breed
+  def name=(name)
+    @name = name
   end
 
   def breed
-    @breed
+    @name
   end
 end
